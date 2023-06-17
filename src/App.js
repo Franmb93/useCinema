8d5c5a888a131b7ec88e9a25d2ee2b2e1a446307
Francisco M. Betanzos <fcomunozbetanzos@gmail.com>
2023-06-17 11:10:21 +0200

    [useCinema] Fetching from OMBD API. Implemented status of loading and error for this fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
+import { useEffect, useState } from "react";
 import NumResults from "./components/navbar/components/NumResults";
 import SearchBar from "./components/navbar/components/SearchBar";
 import Main from "./components/Main";
@@ -7,9 +7,43 @@ import Box from "./components/Box";
 import Movies from "./components/movies/Movies";
 import WatchedMoviesList from "./components/movies/WatchedMoviesList";
 import WatchedSummary from "./components/movies/WatchedSummary";
+import Loader from "./components/Loader";
+import ErrorCustom from "./components/Error";
 function App() {
-  const [movies, setMovies] = useState(tempMovieData);
+  const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState(tempWatchedData);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      setIsLoading(true);
+      try {
+        await fetch(
+          `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMBD_KEY}&s=star-wars`
+        )
+          .then((res) => {
+            if (res.ok) {
+              return res.json();
+            } else {
+              throw new Error("Something went wrong while fetching movies.");
+            }
+          })
+          .then((data) => {
+            if (data.Response === "False") throw new Error(data.Error);
+            setMovies(data.Search);
+          });
+      } catch (error) {
+        console.error(error);
+        setError(error.message);
+        setIsLoading(false);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchMovies();
+  }, []);
 
   return (
     <>
@@ -20,7 +54,9 @@ function App() {
 
       <Main>
         <Box>
-          <Movies movies={movies} />
+          {isLoading && <Loader />}
+          {error && <ErrorCustom error={error} />}
+          {!isLoading && !error && <Movies movies={movies} />}
         </Box>
 
         <Box>
@@ -33,30 +69,6 @@ function App() {
 }
 
 export default App;
-
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
 
 const tempWatchedData = [
   {
---
+++ src/components/Error.js
@@ -0,0 +1,7 @@
+export default function ErrorCustom({ error }) {
+  return (
+    <div className="error">
+      <h1>⛔️{error}</h1>
+    </div>
+  );
+}

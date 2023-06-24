6172dcb5713b834753c4489d581cc4dbf2d39843
Francisco M. Betanzos <fcomunozbetanzos@gmail.com>
2023-06-24 09:34:06 +0200

    [useCinema] Added a focus effect on the searchBar when loading the application

--- src/components/navbar/components/SearchBar.js
+++ src/components/navbar/components/SearchBar.js
@@ -1,4 +1,12 @@
+import { useEffect, useRef } from "react";
+
 export default function SearchBar({ query, setQuery }) {
+  const searchRef = useRef();
+
+  useEffect(function () {
+    searchRef.current.focus();
+  }, []);
+
   return (
     <input
       className="search"
@@ -6,6 +14,7 @@ export default function SearchBar({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={searchRef}
     />
   );
 }
